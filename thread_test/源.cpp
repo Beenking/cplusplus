e39fb2e34840a7461a93ddb9684340d48b7b3852
been_home <bestheart@qq.com>
2017-06-14 22:38:01 +0800

    modify thread test

--- thread_test/源.cpp
+++ thread_test/源.cpp
@@ -4,16 +4,21 @@
 
 void thread_task(int n)
 {
-	std::this_thread::sleep_for(std::chrono::seconds(n));
+	//std::this_thread::sleep_for(std::chrono::seconds(n));
+	int i = 1;
+	while(1)
+	{
+		i = -i;
+	}
 	std::cout << "hello thread " << std::this_thread::get_id() << " paused " << n << " seconds" << std::endl;
 }
 
 int main()
 {
-	std::thread threads[5];
+	std::thread threads[4];
 	std::cout << "Spawing 5 threads...\n";
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 4; i++)
 	{
 		threads[i] = std::thread(thread_task, i + 1);
 	}
