d2391e6fd1bd40cedcc31f2ff63a71413c6890c9
been_home <bestheart@qq.com>
2017-07-01 02:12:13 +0800

    modify auto_install_after.bat

--- dos_batch/auto_install_after.bat
+++ dos_batch/auto_install_after.bat
@@ -1,18 +1,28 @@
-@echo off
+@echo on
 
 @rem change modality
 setlocal enabledelayedexpansion
 set file=a.xml
-set replaced=<Modality>PT</Modality>
-set replacing=<Modality>CT</Modality>
+if not exist %file% (
+	echo %file% missing...
+	pause
+	goto import
+)
+
+set replaced=PT
+set replacing=CT
 
 for /f "delims=" %%i in ('type "%file%"') do (
 	set str=%%i
 	set "str=!str:%replaced%=%replacing%!"
-	echo !str!>>"%file%"_tmp.txt
+	echo !str!>>%file%_tmp.txt
 )
 
 @rem copy "%file%" "%file%"_bak.txt >nul 2>nul
 move "%file%"_tmp.txt "%file%"
 
+pause
 @rem import images
+:import
+echo hello import
+pause
