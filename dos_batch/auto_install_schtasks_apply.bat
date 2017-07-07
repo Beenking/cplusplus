9c5a0d170ef69c0445ee5144588028f777016628
beenkinger <beenkinger@gmail.com>
2017-07-07 10:02:36 +0800

    cancel del schtasks when apply by add para /F

--- dos_batch/auto_install_schtasks_apply.bat
+++ dos_batch/auto_install_schtasks_apply.bat
@@ -1,10 +1,8 @@
 echo off
 @rem set /p password=please input password of %USERNAME%:
 
-SCHTASKS /Delete /TN UIHAutoInstall /F
-SCHTASKS /Create /RU %USERNAME% /RP /TN UIHAutoInstall /XML auto_install.xml
+SCHTASKS /Create /RU %USERNAME% /RP /TN UIHAutoInstall /XML auto_install.xml /F
 @rem SCHTASKS /Change /RU %USERNAME% /RP %password% /TN UIHAutoInstall /TR "%autoInstall% %RemotePackage% %MappingLocalDriver% %IgnoreFailedPackage% %InstallLog% %InstallError% %UihpmLog%"
 
-SCHTASKS /Delete /TN UIHAutoInstallAfter /F
-SCHTASKS /Create /RU %USERNAME% /RP /TN UIHAutoInstallAfter /XML auto_install_after.xml
+SCHTASKS /Create /RU %USERNAME% /RP /TN UIHAutoInstallAfter /XML auto_install_after.xml /F
 @rem SCHTASKS /Change /RU %USERNAME% /RP %password% /TN UIHAutoInstallAfter /TR "%autoInstallAfter% %Replaces% %ImportImages% %AfterLog%"
