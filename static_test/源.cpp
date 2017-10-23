2ab3be1f3991bb4e51bf497bfd1c2f719213a0ea
beenkinger <beenkinger@gmail.com>
2017-10-23 12:50:36 +0800

    static_test

---
+++ static_test/源.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+namespace NS1
+{
+    class A {
+    public:
+        A(int i) 
+        {
+            std::cout << "NS1::A " << i << std::endl;
+        }
+    };
+    static A a1(1);
+}
+
+namespace NS2
+{
+    class B
+    {
+    public:
+        B(int i)
+        {
+            std::cout << "NS2::B " << i << std::endl;
+        }
+    };
+    static B b1(1);
+}
+
+
+static NS1::A a2(2);
+static NS2::B b2(2);
+
+int main()
+{
+    static NS1::A a3(3);
+    static NS2::B b3(3);
+
+    return 0;
+}
