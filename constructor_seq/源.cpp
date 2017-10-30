fd10d8274c4a00f9b83c00b290a8c7de154d8fb6
beenkinger <beenkinger@gmail.com>
2017-10-30 10:46:09 +0800

    class-in member init

--- constructor_seq/源.cpp
+++ constructor_seq/源.cpp
@@ -3,28 +3,32 @@
 class A 
 {
 public:
-    A()
+    A(int i):m_i(i)
     {
         std::cout << "A Constructor\n";
     }
+
+private:
+    int m_i;
 };
 
 
 // 基类构造函数 --> 子类初始化列表 --> 子类成员初始化 --> 子类构造函数体
-class B : public A
+class B 
 {
 public:
-    B()
+    B(int i):m_a1(i)
     {
         std::cout << "B Constructor\n";
     }
 
-private:
-    A m_b;
+private: 
+    A m_a2 = 1;
+    A m_a1;
     int m_i;
 };
 
 int main()
 {
-    B b;
+    B b(2);
 }
