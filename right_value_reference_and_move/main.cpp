5ae23acfe02ec3bb5c9435dc5882c8ec7f13ad79
bin_home <beenkinger@gmail.com>
2018-04-26 00:41:37 +0800

    add time lost

--- right_value_reference_and_move/main.cpp
+++ right_value_reference_and_move/main.cpp
@@ -2,6 +2,7 @@
 #include"right_value_reference_test.h"
 #include <iostream>
 #include <vector>
+#include <ctime>
 
 typedef unsigned char uchar;
 
@@ -12,7 +13,7 @@ class Image
 public:
     Image()
     {
-        cout << "1--Image()..." << endl;
+        //cout << "1--Image()..." << endl;
         m_size = 10 * 1024 * 1024; //10M
         m_pdata = new uchar[m_size];
         memset(m_pdata, 0, m_size);
@@ -20,7 +21,7 @@ public:
 
     Image(const Image& img)
     {
-        cout << "2--Image(const Image&)..." << endl;
+       // cout << "2--Image(const Image&)..." << endl;
         m_size = img.m_size;
         m_pdata = new uchar[m_size];
         memcpy(m_pdata, img.m_pdata, m_size);
@@ -28,7 +29,7 @@ public:
 
     Image& operator= (const Image& img)
     {
-        cout << "operator=(const Image&) " << endl;
+       // cout << "operator=(const Image&) " << endl;
         if (this != &img)
         {
             delete[] m_pdata;
@@ -43,21 +44,21 @@ public:
 
     ~Image()
     {
-        cout << "3--~Image()" << endl;
+        //cout << "3--~Image()" << endl;
         if (m_pdata)
         {
             delete[] m_pdata;
         }
     }
 
-    Image(Image&& img)
-    {
-        cout << "2--Image(const Image&&)" << endl;
-        delete[] m_pdata;
-        m_pdata = img.m_pdata;
-        img.m_pdata = nullptr;
-        img.m_size = 0;
-    }
+    //Image(Image&& img)
+    //{
+    //    //cout << "2--Image(const Image&&)" << endl;
+    //    delete[] m_pdata;
+    //    m_pdata = img.m_pdata;
+    //    img.m_pdata = nullptr;
+    //    img.m_size = 0;
+    //}
 
     //Image& operator=(Image&&)
     //{
@@ -85,12 +86,15 @@ int main()
     // right_value_reference_test();
 
     std::vector<Image> vecImage;
-    vecImage.reserve(5);
 
-    for (int i = 0; i < 5; i++)
+    clock_t start, end;
+    start = clock();
+    for (int i = 0; i < 100; i++)
     {
         vecImage.push_back(Image());
     }
+    end = clock();
+    cout << " Time lost: " << double(end - start)/ CLOCKS_PER_SEC << endl;
 
     return 0;
 }
