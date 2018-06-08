a40ddd9b08554909e10118756b16f010884b568b
beenkinger <beenkinger@gmail.com>
2018-06-08 09:37:10 +0800

    update vs2017

--- All_Test/main.cpp
+++ All_Test/main.cpp
@@ -1,7 +1,33 @@
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <thread>
+#include <memory>
+#include <algorithm>
+#include <exception>
+
+#include "test_and_or.h"
+#include "test.h"
+
+enum TYPE
+{
+    e1,
+    e2 = 1,
+    e3 = 1,
+    e4
+};
+
 
 int main()
 {
-    std::cout << "hello" << std::endl;
+    // test1
+    bool re = (fa() || fb()) && fc();
+
+    A a;
+    a.print();
+
+    return 0;
 }
